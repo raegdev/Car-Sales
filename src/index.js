8ac5bf1887b7fe3da786927fd6b3d5a0061af9db
rae-glazier <glazier.home.life@gmail.com>
2020-03-19 09:41:41 -0500

    actions/reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+const store = createStore();
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
